cf2feb1c212ae474be424db0c458af524ff7cb9d
oets <oets.man@gmail.com>
2025-04-13 15:41:34 +0700

    styled-component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import RestaurantList from '../components/RestaurantsList';
+import Container from '../components/styled/Container';
 
 export default function Home({ restaurants }) {
   return (
-    <div className="container">
+    <Container>
       <RestaurantList restaurants={restaurants} />
-    </div>
+    </Container>
   );
 }
 
